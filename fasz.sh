f4386f3d3875f6f39c468d127cd8e4cfab5cc3c8
lencsa <126465888+lencsa@users.noreply.github.com>
2023-05-12 00:06:55 +0200

    Add files via upload

--- fasz.sh
+++ fasz.sh
@@ -1,20 +1,41 @@
 #!/bin/bash
 
 # Replace "path/to/file" with the path to the file you want to search
-FILE="E:/BME/Projlab/log.log"
+FILE="log.log"
 
-javac -cp log4j-1.2.17.jar: src/*.java
-
-java -cp log4j-1.2.17.jar:src Main
+cat input.txt | java -jar *.jar  #txt végére mindig kell egy exit
 
 
 # Count the number of occurrences of the word in the file and save the result to a variable
-COUNT=$(grep -c "$létrejött" "$FILE")
+
 
 # Check if the count is equal to 45
 echo "Lefutott tesztek: 1/1"
 echo "Pálya létrehozása:"
-if [ $COUNT -eq 45 ]
+
+
+COUNT=$(grep -c "létrejött" "$FILE")
+if [ $COUNT -eq 46 ]
+then
+  echo -e "\033[32mOK\033[0m" #green
+else
+  echo -e "\033[31mFAILED\033[0m" #red
+fi
+
+
+cat input2.txt | java -jar *.jar  #txt végére mindig kell egy exit
+# Count the number of occurrences of the word in the file and save the result to a variable
+
+
+# Check if the count is equal to 45
+echo "Lefutott tesztek: 2/2"
+echo "exit teszt:"
+
+ASD=$(grep -c "exit" "$FILE")
+
+echo $ASD
+
+if [ $ASD -eq 1 ]
 then
   echo -e "\033[32mOK\033[0m" #green
 else
